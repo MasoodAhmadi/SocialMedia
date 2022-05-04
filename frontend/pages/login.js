2673bd001d64fc872856b74da3cfe0314bd9ebed
Masood Ahmadi <masood@eduix.it>
2022-05-04 11:39:05 +0300

    login added

--- frontend/pages/login.js
+++ frontend/pages/login.js
@@ -1,28 +1,94 @@
+import axios from "axios";
+import * as Yup from "yup";
+import { BehaviorSubject } from "rxjs";
+import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
+import { userService } from "../services/user.services";
 import { Form, Button, Message } from "semantic-ui-react";
 import { Segment, TextArea, Divider } from "semantic-ui-react";
-
-//import axios from "axios";
 import { HeaderMessage } from "../components/common/WelcomeMessage";
 import { FooterMessage } from "../components/common/WelcomeMessage";
 
 function Login() {
-  const [user, setUser] = useState({
+  const router = useRouter();
+  /*  const [user, setUser] = useState({
     email: "",
     password: "",
-  });
+  }); */
 
-  const [showPassword, setShowPassword] = useState(false);
   const [errorMsg, setErrorMsg] = useState(false);
   const [formLoading, setFormLoading] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
   const [submitDisable, setSubmitDisable] = useState(true);
+  //const { email, password } = user;
 
-  const { email, password } = user;
+  /* */
+  const [email, setEmail] = useState("");
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState("");
+  const [render, setRender] = useState(0);
+  const [password, setPassword] = useState("");
+  /*  */
 
   const handleChange = (e) => {
     const { name, value } = e.target;
 
     setUser((prev) => ({ ...prev, [name]: value }));
+  };
+  const validationSchema = Yup.object().shape({
+    username: Yup.string().required("Username is required"),
+    password: Yup.string().required("Password is required"),
+  });
+
+  useEffect(() => {
+    // redirect to home if already logged in
+    if (userService.userValue) {
+      router.push("/");
+    }
+  }, []);
+
+  const getUser = async () => {
+    try {
+      const { data } = await axios.get(getUserByTokenUrl, {
+        headers: {
+          "x-auth-token": localStorage.token,
+        },
+      });
+      setUser(data);
+    } catch (error) {
+      localStorage.removeItem("token");
+      user && errorToast("Session expired");
+      setUser(null);
+    }
+  };
+
+  useEffect(() => {
+    if (localStorage.token) {
+      getUser();
+    }
+    rerender(render, setRender);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [render]);
+
+  const handleSumbit = async (e) => {
+    e.preventDefault();
+    const { value, error: err } = validationSchema.validate({
+      email,
+      password,
+    });
+    if (err) return setError(err.details[0].message);
+    try {
+      const { data } = await axios.post(
+        "http://localhost:8000/api/users/signin",
+        value
+      );
+      localStorage.setItem("token", data.token);
+      if (!err) setError("");
+      data && router.push("/signin");
+      getUser();
+    } catch (error) {
+      error.response && setError(error.response.data.error);
+    }
   };
 
   useEffect(() => {
@@ -32,17 +98,17 @@ function Login() {
     isUser ? setSubmitDisable(false) : setSubmitDisable(true);
   }, [user]);
 
-  const handleSumbit = (e) => {
-    e.preventDefault();
-  };
+  const userSubject = new BehaviorSubject(
+    process.browser && JSON.parse(localStorage.getItem("user"))
+  );
 
   const login = (username, password) => {
-    return fetchWrapper
-      .post(`${baseUrl}/authenticate`, { username, password })
+    axios
+      .post("http://localhost:8000/api/users/signin", { username, password })
       .then((user) => {
         // publish user to subscribers and store in local storage to stay logged in between page refreshes
         userSubject.next(user);
-        localStorage.setItem("user", JSON.stringify(user));
+        localStorage.setItem("user", JSON.stnpmringify(user));
 
         return user;
       });
@@ -68,7 +134,7 @@ function Login() {
           <Form.Input
             label="Email"
             name="email"
-            placeholdeR="type you email"
+            placeholder="type you email"
             value={email}
             onChange={handleChange}
             fluid
@@ -80,7 +146,7 @@ function Login() {
           <Form.Input
             label="Password"
             name="password"
-            placeholdeR="type you password"
+            placeholder="type you password"
             value={password}
             onChange={handleChange}
             fluid
---
+++ frontend/services/user.services.js
@@ -0,0 +1,39 @@
+import { BehaviorSubject } from "rxjs";
+import Router from "next/router";
+
+const userSubject = new BehaviorSubject(
+  process.browser && JSON.parse(localStorage.getItem("user"))
+);
+
+export const userService = {
+  user: userSubject.asObservable(),
+  get userValue() {
+    return userSubject.value;
+  },
+  login,
+  logout,
+  getAll,
+};
+
+function login(username, password) {
+  return fetchWrapper
+    .post("https://localhost:8000/api/users/signin", { username, password })
+    .then((user) => {
+      // publish user to subscribers and store in local storage to stay logged in between page refreshes
+      userSubject.next(user);
+      localStorage.setItem("user", JSON.stringify(user));
+
+      return user;
+    });
+}
+
+function logout() {
+  // remove user from local storage, publish null to user subscribers and redirect to login page
+  localStorage.removeItem("user");
+  userSubject.next(null);
+  Router.push("/login");
+}
+
+function getAll() {
+  return axios.get("https://localhost:8000/api/users/signin");
+}
