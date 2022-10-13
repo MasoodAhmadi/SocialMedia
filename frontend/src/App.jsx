06bcb8a51a9ec6b7a3d3f0102b6d93aa55b1cbb4
Masood Ahmadi <masood@eduix.it>
2022-10-13 15:11:13 +0300

    react redux branch is added

---
+++ frontend/src/App.jsx
@@ -0,0 +1,77 @@
+// import { unwrapResult } from "@reduxjs/toolkit";
+import React from "react";
+import { Container, Row } from "react-bootstrap";
+import { IntlProvider } from "react-intl";
+import { useDispatch } from "react-redux";
+import {
+  Redirect,
+  Route,
+  Switch,
+  useHistory,
+  useLocation,
+} from "react-router-dom";
+import homePage from "./pages/home.page";
+
+export default function App() {
+  // const dispatch = useDispatch();
+  // const location = useLocation();
+  // const configState = useSelector((state) => state.config);
+  // const localizationsState = useSelector((state) => state.localizations);
+  // const history = useHistory();
+
+  // const isRootPage = location.pathname === "/";
+
+  // useEffect(() => {
+  //   if (!isRootPage) {
+  //     dispatch(loadTags());
+  //     dispatch(loadDegrees());
+  //     dispatch(loadTypes());
+  //     loadUser();
+  //   }
+
+  //   const unListenHistory = history.listen((location) => {
+  //     if (
+  //       localStorage.getItem("access-token") &&
+  //       location.pathname.includes("/admin")
+  //     ) {
+  //       unwrapResult(dispatch(getUserInfo()));
+  //     }
+  //   });
+
+  //   return () => {
+  //     unListenHistory();
+  //   };
+  // }, []);
+
+  return (
+    <>
+      <IntlProvider
+      // locale={localizationsState.locale}
+      // messages={localizationsState.data}
+      >
+        <div>
+          <Container
+            fluid
+            style={{
+              minHeight: "calc(100vh - 4rem)",
+            }}
+          >
+            <Row style={{ paddingTop: "5rem", paddingBottom: "0" }}></Row>
+            <main
+              style={{
+                paddingBottom: "5rem",
+                paddingTop: "1rem",
+              }}
+            >
+              <Switch>
+                <Route exact path="/" component={homePage} />
+                {/* <Route component={NotFoundPage} /> */}
+                {/* <Redirect to={configState.organizationUrl || "/"} /> */}
+              </Switch>
+            </main>
+          </Container>
+        </div>
+      </IntlProvider>
+    </>
+  );
+}
---
+++ frontend/src/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import * as serviceWorker from "./serviceWorker";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { HashRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+
+ReactDOM.render(
+  <HashRouter>
+    <App />
+  </HashRouter>,
+  document.getElementById("root")
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+reportWebVitals();
+serviceWorker.register({
+  onUpdate: () => {
+    console.log("Update happened.");
+  },
+});
