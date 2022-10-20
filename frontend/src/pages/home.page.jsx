a094ccea4db4bf6526ed1f8638cfab0d80d488a8
Masood Ahmadi <masood@eduix.it>
2022-10-20 15:52:25 +0300

    design changed main page

--- frontend/src/pages/home.page.jsx
+++ frontend/src/pages/home.page.jsx
@@ -1,39 +1,10 @@
 import React from "react";
-import { Row, Container, ListGroup } from "react-bootstrap";
-import Example from "../components/coursal";
-import WeekofTheDay from "../components/week";
+import WeekofTheDay from "../components/weekofTheDay";
 
 export default function homePage() {
   return (
     <div>
-      <>
-        <Row>
-          <div
-            style={{
-              width: "100%",
-              // position: "relative",
-              // display: "flex",
-              // justifyContent: "center",
-              // alignItems: "center",
-            }}
-          >
-            <Example />
-          </div>
-        </Row>
-        <Row>
-          <div
-            style={{
-              width: "100%",
-              // position: "relative",
-              // display: "flex",
-              // justifyContent: "center",
-              // alignItems: "center",
-            }}
-          >
-            <WeekofTheDay />
-          </div>
-        </Row>
-      </>
+      <WeekofTheDay />
     </div>
   );
 }
