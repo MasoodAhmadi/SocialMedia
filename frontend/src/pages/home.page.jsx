02c1404723d1e70c9960c55332e013d92627ea7b
Masood Ahmadi <masood@eduix.it>
2022-10-25 13:32:40 +0300

    listgroup component added

--- frontend/src/pages/home.page.jsx
+++ frontend/src/pages/home.page.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import ListGroups from "../components/listGroup";
 import WeekofTheDay from "../components/weekofTheDay";
 
 export default function homePage() {
   return (
     <div>
       <WeekofTheDay />
+      <br></br>
+      <ListGroups />
     </div>
   );
 }
