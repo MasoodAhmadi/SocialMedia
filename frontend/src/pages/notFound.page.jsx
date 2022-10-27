8f8f97e5bf6e36f434771018fd0e700ad833cd00
Masood Ahmadi <masood@eduix.it>
2022-10-27 11:25:23 +0300

    changed applied

--- frontend/src/pages/notFound.page.jsx
+++ frontend/src/pages/notFound.page.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Button, Col, Container, Row } from "react-bootstrap";
 import { useHistory } from "react-router-dom";
-
+// is comcone
 export default function NotFoundPage() {
   const history = useHistory();
   return (
