e8f57a45b996cfecc37b0a18a7241aee8bf52780
Masood Ahmadi <masood@eduix.it>
2023-05-26 17:41:08 +0300

    login page modification

--- frontend/src/pages/signup.page..jsx
+++ frontend/src/pages/signup.page..jsx
@@ -9,18 +9,18 @@ import { Alert, Card, Col, Form, Row } from 'react-bootstrap';
 import { Container } from 'react-bootstrap';
 
 import {
-  FacebookButton,
-  ForgotPasswordButton,
-  LoginButton,
-  LoginFormContainer,
+  ButtonContainer,
+  ForgotPassword,
+  HorizontalRule,
+  IconsContainer,
+  InputContainer,
+  MainContainer,
+  TextInput,
 } from '../styles/identify.styles';
 import {
   FooterMessage,
   HeaderMessage,
 } from '../components/common/WelcomeMessage';
-import { CardImage, Facebook } from 'react-bootstrap-icons';
-import styled from 'styled-components';
-import { Link, useHistory } from 'react-router-dom';
 import Input from '../components/common/Input.component';
 import Button from '../components/common/button';
 import Icon from '../components/common/Icon.component';
@@ -30,7 +30,10 @@ import { useDispatch } from 'react-redux';
 import { routes } from '../config';
 
 function SignupPage() {
+  const { link } = routes;
   const inputRef = useRef();
+  const dispatch = useDispatch();
+
   const [bio, setBio] = useState('');
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
@@ -51,43 +54,45 @@ function SignupPage() {
     instagram: '',
     twitter: '',
   });
-  const dispatch = useDispatch();
-  const { link } = routes;
-
   const [authMode, setAuthMode] = useState('identify');
 
-  // const handleChange = (e) => {
-  //   const { name, value } = e.target;
-  //   setAllSocialState((prev) => ({ ...prev, [name]: value }));
-  // };
-
   const userSchema = Yup.object({
+    name: Yup.string().required('Required!'),
+    latname: Yup.string().required('Required!'),
+    username: Yup.string().required('Required!'),
+    bio: Yup.string().min(255, 'Maximum 255 characters').required('Required!'),
     email: Yup.string().email('Invalid email format').required('Required!'),
     password: Yup.string().min(5, 'Minimum 5 characters').required('Required!'),
   });
 
   const formik = useFormik({
     initialValues: {
+      name: '',
+      latname: '',
+      username: '',
       email: '',
       password: '',
+      bio: '',
     },
     validationSchema: userSchema,
-    onSubmit: async (values) => {
+    onSubmit: async ({ name, latname, username, email, password, bio }) => {
       try {
-        const response = unwrapResult(await dispatch(signInUser(values)));
-        history.push(link);
+        unwrapResult(
+          await dispatch(
+            addUser({ name, latname, username, email, password, bio })
+          )
+        );
+        // dispatch(closeModal());
         dispatch(
           addNotification({
-            identifier: 'user',
-            timeout: 5,
-            icon: <UnlockFill className='text-success' />,
-            content: response?.name
-              ? `${localize('Welcome')} ${response?.name}`
-              : localize('Welcome'),
+            icon: <ClipboardCheck size={25} className='me-2' />,
+            content: 'user created sucessfully',
+            timeout: 3,
           })
         );
       } catch (error) {
-        console.log('error: ', error);
+        // dispatch(closeModal());
+        console.error(error.message);
       }
     },
   });
@@ -153,6 +158,13 @@ function SignupPage() {
               <Form onSubmit={formik.handleSubmit}>
                 <InputContainer>
                   <Input
+                    type='name'
+                    name='name'
+                    placeholder='Name'
+                    value={formik.values.name}
+                    onChange={formik.handleChange}
+                  />
+                  <Input
                     type='email'
                     name='email'
                     placeholder='Email'
@@ -179,6 +191,20 @@ function SignupPage() {
                       {formik.errors.password}
                     </p>
                   )}
+                  <Input
+                    type='text'
+                    placeholder='Username'
+                    name='username'
+                    value={formik.values.username}
+                    onChange={formik.handleChange}
+                  />
+                  <Input
+                    type='text'
+                    placeholder='Bio'
+                    name='bio'
+                    value={formik.values.bio}
+                    onChange={formik.handleChange}
+                  />
                 </InputContainer>
                 <ButtonContainer>
                   <Button type='submit' content='Sign in' />
@@ -202,340 +228,10 @@ function SignupPage() {
           </Col>
         </Row>
       </Container>
-      {/* <LoginFormContainer>
-        <Form loading={formLoading} error={errorMsg !== null}>
-          <Row className='mt-1'>
-            <Col>
-              <HeaderMessage />
-              <Alert
-                color='warmmidgrey'
-                error
-                header='Oops!'
-                content={errorMsg}
-                onDismiss={() => setErrorMsg(null)}
-              >
-                <div>
-                  <Alert.Heading style={{ fontSize: '1rem' }}></Alert.Heading>
-                </div>
-              </Alert>
-              <Form>
-                <Card>
-                  <div>
-                    <input
-                      style={{ display: 'none' }}
-                      type='file'
-                      accept='image/*'
-                      onChange={uploadToClient}
-                      name='media'
-                      ref={inputRef}
-                    />
-                    <div
-                      onDragOver={(e) => {
-                        e.preventDefault();
-                        setHighlighed(true);
-                      }}
-                      onDragLeave={(e) => {
-                        e.preventDefault();
-                        setHighlighed(false);
-                      }}
-                      onDrop={(e) => {
-                        e.preventDefault();
-                        setHighlighed(true);
-                        const droppedFile = Array.from(e.dataTransfer.files);
-                        setMedia(droppedFile[0]);
-                        setCreateObjectURL(URL.createObjectURL(droppedFile[0]));
-                        console.log(e.dataTransfer.files);
-                      }}
-                    >
-                      {createObjectURL === null ? (
-                        <div
-                          style={
-                            highlighted
-                              ? { backgroundColor: '#D0F0C0' }
-                              : { background: '' }
-                          }
-                        >
-                          <div className='d-flex justify-content-center'>
-                            <CardImage
-                              className='d-flex justify-content-center'
-                              name='file image outline'
-                              style={{
-                                cursor: 'pointer',
-                              }}
-                              onClick={() => inputRef.current.click()}
-                            />
-                          </div>
-                          <h4
-                            className='d-flex justify-content-center'
-                            onClick={() => inputRef.current.click()}
-                          >
-                            Drag n drop or click to upload image
-                          </h4>
-                        </div>
-                      ) : (
-                        <div>
-                          <div className='d-flex justify-content-center my-3'>
-                            <Button
-                              onClick={() => inputRef.current.click()}
-                              variant='none'
-                              className='position-relative p-0 border-0'
-                            >
-                              <img
-                                src={createObjectURL}
-                                className='rounded w-100'
-                              />
-                            </Button>
-                          </div>
-                        </div>
-                      )}
-                    </div>
-                  </div>
-                </Card>
-              </Form>
-
-              <Form.Group className='mb-1' controlId='formBasicEmail'>
-                <Form.Label>Name</Form.Label>
-                <Form.Control
-                  name='email'
-                  type='email'
-                  placeholder='Enter email'
-                  value={formik.values.email}
-                  onChange={formik.handleChange}
-                />
-                <Form.Text className='text-muted'>
-                  We'll never share your email with anyone.
-                </Form.Text>
-                {formik.errors.email && formik.touched.email && (
-                  <p className='mt-2' style={{ color: basic.dark }}>
-                    {formik.errors.email}
-                  </p>
-                )}
-              </Form.Group>
-              <Form.Group className='mb-1' controlId='formBasicEmail'>
-                <Form.Label>Email</Form.Label>
-                <Form.Control
-                  name='email'
-                  type='email'
-                  placeholder='Enter email'
-                  value={formik.values.email}
-                  onChange={formik.handleChange}
-                />
-                <Form.Text className='text-muted'>
-                  We'll never share your email with anyone.
-                </Form.Text>
-                {formik.errors.email && formik.touched.email && (
-                  <p className='mt-2' style={{ color: basic.dark }}>
-                    {formik.errors.email}
-                  </p>
-                )}
-              </Form.Group>
-              <Form.Group className='mb-3' controlId='formBasicPassword'>
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                  name='password'
-                  type='password'
-                  placeholder='Enter password'
-                  value={formik.values.password}
-                  onChange={formik.handleChange}
-                  aria-label='Password for access admin'
-                />
-                {formik.errors.password && formik.touched.password && (
-                  <p className='' style={{ color: 'black' }}>
-                    {formik.errors.password}
-                  </p>
-                )}
-              </Form.Group>
-              <Form.Group className='mb-3' controlId='formBasicPassword'>
-                <Form.Label>Username</Form.Label>
-                <Form.Control
-                  name='username'
-                  type='text'
-                  placeholder='username'
-                  value={formik.values.password}
-                  onChange={formik.handleChange}
-                  aria-label='user for access admin'
-                />
-                {formik.errors.password && formik.touched.password && (
-                  <p className='' style={{ color: 'black' }}>
-                    {formik.errors.password}
-                  </p>
-                )}
-              </Form.Group>
-              <Form.Group className='mb-3' controlId='formBasicPassword'>
-                <Form.Label>Bio</Form.Label>
-                <Form.Control
-                  name='username'
-                  type='text'
-                  placeholder='username'
-                  value={formik.values.password}
-                  onChange={formik.handleChange}
-                  aria-label='user for access admin'
-                />
-                {formik.errors.password && formik.touched.password && (
-                  <p className='' style={{ color: 'black' }}>
-                    {formik.errors.password}
-                  </p>
-                )}
-              </Form.Group>
-            </Col>
-          </Row>
-
-          <Row>
-            <Col xs={6} sm={6} md={6} lg={6} xl={6}>
-              <LoginButton type='submit'>login</LoginButton>
-            </Col>
-            <Col xs={6} sm={6} md={6} lg={6} xl={6}>
-              <ForgotPasswordButton>forgot password</ForgotPasswordButton>
-            </Col>
-            <FooterMessage />
-          </Row>
-          <Row>
-            <Col></Col>
-          </Row>
-          <div>
-            <FacebookButton>
-              <Row>
-                <Col xs={9} sm={9} md={9} lg={9} xl={9}>
-                  sign in with
-                </Col>
-                <Col xs={3} sm={3} md={3} lg={3} xl={3}>
-                  <Facebook width={22} height={22} />
-                </Col>
-              </Row>
-            </FacebookButton>
-            <FacebookButton>
-              {' '}
-              <Row>
-                {' '}
-                <Col xs={9} sm={9} md={9} lg={9} xl={9}>
-                  sign in with
-                </Col>
-                <Col xs={3} sm={3} md={3} lg={3} xl={3}>
-                  <Facebook width={22} height={22} />
-                </Col>
-              </Row>
-            </FacebookButton>
-          </div>
-          <br />
-        </Form>
-      </LoginFormContainer> */}
     </>
   );
 }
 export default SignupPage;
-
-const TextInput = styled.p`
-  color: black;
-  text-transform: lowercase;
-  display: flex;
-  align-items: center;
-`;
-const MainContainer = styled.div`
-  // display: flex;
-  align-items: center;
-  // justify-content: center;
-  flex-direction: column;
-  height: 80vh;
-  width: 20vw;
-  background: rgba(255, 255, 255, 0.15);
-  box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
-  backdrop-filter: blur(8.5px);
-  -webkit-backdrop-filter: blur(8.5px);
-  border-radius: 10px;
-  color: #ffffff;
-  text-transform: uppercase;
-  letter-spacing: 0.1rem;
-  @media only screen and (max-width: 320px) {
-    width: 80vw;
-    height: 60vh;
-    hr {
-      margin-bottom: 0.3rem;
-    }
-    h4 {
-      font-size: small;
-    }
-  }
-  @media only screen and (min-width: 360px) {
-    width: 80vw;
-    height: 87vh;
-    h4 {
-      font-size: small;
-    }
-  }
-  @media only screen and (min-width: 411px) {
-    width: 80vw;
-    height: 60vh;
-  }
-
-  @media only screen and (min-width: 768px) {
-    width: 80vw;
-    height: 60vh;
-  }
-  @media only screen and (min-width: 1024px) {
-    width: 60vw;
-    height: 60vh;
-  }
-  @media only screen and (min-width: 1280px) {
-    width: 30vw;
-    height: 61vh;
-  }
-`;
-
-const WelcomeText = styled.h2`
-  margin: 3rem 0 2rem 0;
-  color: black;
-`;
-
-const InputContainer = styled.div`
-  display: flex;
-  // margin: 10px;
-  padding-top: 5rem;
-  flex-direction: column;
-  justify-content: space-around;
-  align-items: center;
-  height: 20%;
-  width: 100%;
-`;
-
-const ButtonContainer = styled.div`
-  margin: 1rem 0 2rem 0;
-  width: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-
-const LoginWith = styled.p`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  color: black;
-`;
-
-const HorizontalRule = styled.hr`
-  width: 100%;
-  height: 0.3rem;
-  border-radius: 0.8rem;
-  border: none;
-  background: linear-gradient(to right, #14163c 0%, #03217b 79%);
-  background-color: #ebd0d0;
-  margin: 1.5rem 0 1rem 0;
-  backdrop-filter: blur(25px);
-`;
-
-const IconsContainer = styled.div`
-  display: flex;
-  justify-content: space-evenly;
-  margin: 2rem 0 3rem 0;
-  width: 100%;
-`;
-
-const ForgotPassword = styled(Link)`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  color: black;
-`;
 
 // import React, { useEffect, useRef, useState } from "react";
 // import { Segment, TextArea, Divider } from "semantic-ui-react";
@@ -811,83 +507,3 @@ const ForgotPassword = styled(Link)`
 //   );
 // }
 // export default Signup;
-
-{
-  /* <div>
-          <h4>name</h4>
-          <input
-            type="text"
-            name="name"
-            onChange={(e) => setName(e.target.value)}
-          />
-        </div> */
-}
-{
-  /* <div>
-          <h4>username</h4>
-          <input
-            type="text"
-            name="username"
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </div> */
-}
-{
-  /* <div>
-          <h4>password</h4>
-          <input
-            type="password"
-            name="password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div> */
-}
-{
-  /* <div>
-          <h4>email</h4>
-          <input
-            type="email"
-            name="email"
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div> */
-}
-{
-  /* <div>
-          <h4>bio</h4>
-          <input
-            type="text"
-            name="bio"
-            onChange={(e) => setBio(e.target.value)}
-          />
-        </div> */
-}
-{
-  /* <button
-          className="btn btn-primary"
-          type="submit"
-          onClick={async () => {
-            await router.push("/home");
-            await onSubmit();
-          }}
-        >
-          Create User
-        </button> */
-}
-{
-  /* <div>
-          <img src={createObjectURL} />
-          <h4>Select Image</h4>
-          <input type="file" name="myImage" onChange={uploadToClient} />
-          <button
-            className="btn btn-primary"
-            type="submit"
-            onClick={async () => {
-              await router.push("/home");
-              await onSubmit();
-            }}
-          >
-            Create User
-          </button>
-        </div> */
-}
--- frontend/src/styles/identify.styles.js
+++ frontend/src/styles/identify.styles.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 export const LoginFormContainer = styled.div`
   z-index: 99;
@@ -74,4 +75,118 @@ export const LogoutButton = styled(Button)`
     color: ${({ theme }) => theme.basic.greyOpacity};
     // background-color: ${({ theme }) => theme.primary};
   }
+`;
+
+export const TextInput = styled.p`
+  color: black;
+  text-transform: lowercase;
+  display: flex;
+  align-items: center;
+`;
+export const MainContainer = styled.div`
+  // display: flex;
+  align-items: center;
+  // justify-content: center;
+  flex-direction: column;
+  height: 80vh;
+  width: 20vw;
+  background: rgba(255, 255, 255, 0.15);
+  box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+  backdrop-filter: blur(8.5px);
+  -webkit-backdrop-filter: blur(8.5px);
+  border-radius: 10px;
+  color: #ffffff;
+  text-transform: uppercase;
+  letter-spacing: 0.1rem;
+  @media only screen and (max-width: 320px) {
+    width: 80vw;
+    height: 60vh;
+    hr {
+      margin-bottom: 0.3rem;
+    }
+    h4 {
+      font-size: small;
+    }
+  }
+  @media only screen and (min-width: 360px) {
+    width: 80vw;
+    height: 87vh;
+    h4 {
+      font-size: small;
+    }
+  }
+  @media only screen and (min-width: 411px) {
+    width: 80vw;
+    height: 60vh;
+  }
+
+  @media only screen and (min-width: 768px) {
+    width: 80vw;
+    height: 60vh;
+  }
+  @media only screen and (min-width: 1024px) {
+    width: 60vw;
+    height: 60vh;
+  }
+  @media only screen and (min-width: 1280px) {
+    width: 30vw;
+    height: 71vh;
+  }
+`;
+
+export const WelcomeText = styled.h2`
+  margin: 3rem 0 2rem 0;
+  color: black;
+`;
+
+export const InputContainer = styled.div`
+  display: flex;
+  margin: 2px;
+  gap: 10px;
+  padding-top: 5rem;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  height: 20%;
+  width: 100%;
+`;
+
+export const ButtonContainer = styled.div`
+  margin: 1rem 0 2rem 0;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const LoginWith = styled.p`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: black;
+`;
+
+export const HorizontalRule = styled.hr`
+  width: 100%;
+  height: 0.3rem;
+  border-radius: 0.8rem;
+  border: none;
+  background: linear-gradient(to right, #14163c 0%, #03217b 79%);
+  background-color: #ebd0d0;
+  margin: 1.5rem 0 1rem 0;
+  backdrop-filter: blur(25px);
+`;
+
+export const IconsContainer = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  margin: 2rem 0 3rem 0;
+  width: 100%;
+`;
+
+export const ForgotPassword = styled(Link)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: black;
 `;
