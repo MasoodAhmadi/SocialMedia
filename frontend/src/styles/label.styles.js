05a277297b33daf395e3398dd51e2b726da787a9
Masood Ahmadi <masood@eduix.it>
2023-06-26 17:34:45 +0300

    login page design changed

---
+++ frontend/src/styles/label.styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+import { FormLabel } from 'react-bootstrap';
+
+export const StyledLabel = styled(FormLabel)`
+  // background: rgba(255, 255, 255, 0.15);
+  // box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+ 
+  border: none;
+  outline: none;
+	color: #656565;
+	font-size: 1rem;
+
+ 
+`;
