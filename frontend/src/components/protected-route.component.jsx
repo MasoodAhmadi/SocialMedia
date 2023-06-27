0bb5577d6eec5a169e5b1b7924849fef083618f7
Masood Ahmadi <masood@eduix.it>
2023-06-27 13:08:45 +0300

    protected route added

--- frontend/src/components/protected-route.component.jsx
+++ frontend/src/components/protected-route.component.jsx
@@ -1,30 +1,30 @@
-// import React from 'react';
-// import { Redirect, Route } from 'react-router-dom';
-// import { routes } from '../config';
-// import authServices from '../services/auth.services';
+import React from 'react';
+import { Redirect, Route } from 'react-router-dom';
+import { routes } from '../config';
+import authServices from '../services/auth.services';
 
-// export default function ProtectedRoute({
-//   path,
-//   component: Component,
-//   ...rest
-// }) {
-//   const { identify, home, link } = routes;
-//   return (
-//     <Route
-//       path={path}
-//       {...rest}
-//       render={(props) => {
-//         if (!authServices.getCurrentUser())
-//           return (
-//             <Redirect
-//               to={{
-//                 pathname: home,
-//                 state: { from: props.location },
-//               }}
-//             />
-//           );
-//         return Component ? <Component {...props} /> : render(props);
-//       }}
-//     />
-//   );
-// }
+export default function ProtectedRoute({
+  path,
+  component: Component,
+  ...rest
+}) {
+  const { identify } = routes;
+  return (
+    <Route
+      path={path}
+      {...rest}
+      render={(props) => {
+        if (!authServices.getCurrentUser())
+          return (
+            <Redirect
+              to={{
+                pathname: identify,
+                state: { from: props.location },
+              }}
+            />
+          );
+        return Component ? <Component {...props} /> : render(props);
+      }}
+    />
+  );
+}
