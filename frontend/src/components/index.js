c011d87453e005ea958c6454667f3e8e8db0ea99
Masood Ahmadi <masood@eduix.it>
2023-06-27 13:05:44 +0300

    styles updated

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -4,6 +4,3 @@ export { default as Navbars } from './navbar.component';
 export { default as ListGroups } from './listGroup';
 export { default as WeekofTheDay } from './week-day.component';
 export { default as NotificationManager } from './common/snacbar.component';
-// export { default as ProtectedRoute } from './protected-route.component';
-// export { default as FooterMessage } from './common/WelcomeMessage';
-// export { default as HeaderMessage } from './common/WelcomeMessage';
